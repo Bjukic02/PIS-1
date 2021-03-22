690a477d3ff72a38f5f171321341d2c2a0558b62
Bjukic02 <Bozidar.Jukic.02@fesb.hr>
2021-03-22 17:38:06 +0100

    modified:   bjukic02Pis/Program.cs

--- bjukic02Pis/Program.cs
+++ bjukic02Pis/Program.cs
@@ -7,6 +7,7 @@ namespace bjukic02Pis
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Druga poruka");
         }
     }
 }
